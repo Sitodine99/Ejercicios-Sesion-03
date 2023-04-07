5da5cea9a7787c94e587b51d23540e12e747c3b5
Jose Antonio Montero <josamontero@gmail.com>
2023-04-07 16:53:03 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ejercicio_Sesion_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente miCliente = new Cliente
+            {
+                NombreCompleto = "Jose Antonio",
+                Telefono = "665376263",
+                Direccion = "Calle 123, Madrid",
+                Email = "jose.antonio@example.com",
+                EsNuevoCliente = true
+            };
+
+            Console.WriteLine(miCliente.ToString());
+        }
+    }
+}
+public struct Cliente
+{
+    public string NombreCompleto { get; set; }
+    public string Telefono { get; set; }
+    public string Direccion { get; set; }
+    public string Email { get; set; }
+    public bool EsNuevoCliente { get; set; }
+
+    public override string ToString()
+    {
+        return $"Nombre completo: {NombreCompleto}\nTeléfono: {Telefono}\nDirección: {Direccion}\nEmail: {Email}\n¿Es nuevo cliente?: {EsNuevoCliente}";
+    }
+}
